f6041c0adeeddc49aa0ffa4fddee7858314fe8b7
Kylir Horton <kylirh@gmail.com>
2018-04-19 14:19:30 -0600

    Clean up heroes list

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Heroes from './components/Heroes/Heroes';
 class App extends Component {
   render() {
     const heroList = [
-      { id: 1, name: "Konrad Sanro", persona: "Retwoner", active: false, selected: false, },
-      { id: 2, name: "Rowan Bredal", persona: "Hyprona", active: false, selected: false, },
-      { id: 3, name: "Ulani Kywann", persona: "The Archkay", active: false, selected: false, },
-      { id: 4, name: "Broadrick Ikon", persona: "E-NINJA", active: false, selected: false, },
-      { id: 5, name: "Stuard Jedrek", persona: "Blades", active: false, selected: false, },
-      { id: 6, name: "Bruce Wayne", persona: "Batman", active: true, selected: false, },
-      { id: 7, name: "Clark Kent", persona: "Superman", active: true, selected: false, },
-      { id: 8, name: "Logan", persona: "Wolverine", active: true, selected: false, },
-      { id: 9, name: "Peter Parker", persona: "Spider Man", active: true, selected: false, },
-      { id: 10, name: "Tony Stark", persona: "Iron Man", active: true, selected: false, },
+      { id: 1, name: "Konrad Sanro", persona: "Retwoner", active: false },
+      { id: 2, name: "Rowan Bredal", persona: "Hyprona", active: false },
+      { id: 3, name: "Ulani Kywann", persona: "The Archkay", active: false },
+      { id: 4, name: "Broadrick Ikon", persona: "E-NINJA", active: false },
+      { id: 5, name: "Stuard Jedrek", persona: "Blades", active: false },
+      { id: 6, name: "Bruce Wayne", persona: "Batman", active: true },
+      { id: 7, name: "Clark Kent", persona: "Superman", active: true },
+      { id: 8, name: "Logan", persona: "Wolverine", active: true },
+      { id: 9, name: "Peter Parker", persona: "Spider Man", active: true },
+      { id: 10, name: "Tony Stark", persona: "Iron Man", active: true },
     ];
 
     return (
